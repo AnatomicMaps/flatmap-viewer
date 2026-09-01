fc3c8fdd5b2cdf35ff851c5c3f32425e003bfbe3
David Brooks <dbrnz@users.noreply.github.com>
2026-09-01 14:41:41 +1200

    config: add a `clean` script.

---
+++ scripts/clean.js
@@ -0,0 +1,11 @@
+import fs from 'node:fs'
+
+for (const path of [
+    'dist',
+    'node_modules',
+    'src/assets/oxigraph'
+]) {
+    if (fs.existsSync(path)) {
+        fs.rmSync(path, { recursive: true, force: true })
+    }
+}
